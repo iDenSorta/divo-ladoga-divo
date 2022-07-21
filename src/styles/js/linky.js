3001eec8b2f69c03773ec7599b2e1355b17c0413
iDenSorta <denispanyukov@gmail.com>
2022-07-21 21:36:47 +0300

    popup height and scroll, bg fix

--- src/styles/js/linky.js
+++ src/styles/js/linky.js
@@ -1,6 +1,6 @@
 
 // Слушаем наведение на блоки с домами или турами, чтобы при нажатии на них выполнить нажатие на кнопку в данном блоке.
-ocument.querySelectorAll('.info-item').forEach(item => {
+document.querySelectorAll('.info-item').forEach(item => {
     item.addEventListener('click', event => {
                
         item.querySelector('a').click();
