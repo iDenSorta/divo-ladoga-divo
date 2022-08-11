f2ef436af0e3a2e465dccfe4458f3d68e9ec2f0a
iDenSorta <denispanyukov@gmail.com>
2022-08-11 18:09:24 +0300

    fixed popup close_cross and new switchable text block

--- src/styles/js/switch.js
+++ src/styles/js/switch.js
@@ -10,13 +10,21 @@ for (var i=0; i<radio.length; i++){
 
 function testRadio(){
     let mas = document.getElementById('switch');
+    let txt_left = document.getElementById('txt-left');
+    let txt_right = document.getElementById('txt-right');
     
 
     if (mas.className == 'routes-img-wrapper left'){
         mas.className = 'routes-img-wrapper right';
+        txt_left.className = 'routes-text-wrapper route-txt';
+        txt_right.className = 'routes-text-wrapper';
     }
     else {
-    mas.className = 'routes-img-wrapper left';}
+    mas.className = 'routes-img-wrapper left';
+    txt_right.className = 'routes-text-wrapper route-txt';
+    txt_left.className = 'routes-text-wrapper';
+
+}
     
 }
 
